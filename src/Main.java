2e871e6b057e5577d4419a9d35d3521622b62fc4
shalantor <georgkaraolanis@gmail.com>
2018-03-11 15:12:32 +0200

    Implement calculation of block size

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        TransactionManager transaction = new TransactionManager();
+        TransactionManager manager = new TransactionManager();
 
 
         /*Test block*/
         ArrayList<HashMap<String,Object>> transactions = new ArrayList<>();
         for(int i =0; i < 10; i ++){
-            transactions.add(transaction.createRandomTransaction());
+            transactions.add(manager.createRandomTransaction());
         }
         Block block = new Block(1,"qwe",transactions);
         Block block2 = new Block(1,"qwe",transactions);
--- src/structure/Block.java
+++ src/structure/Block.java
@@ -2,11 +2,8 @@ package structure;
 
 /*The data structure for the block*/
 
-import com.sun.xml.internal.ws.api.message.Message;
-
 import javax.xml.bind.DatatypeConverter;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -28,7 +25,8 @@ public class Block {
     public int numTransactions;
     public List<HashMap<String,Object>> transactions;
 
-    public Block(long index, String previousBlockHash, List<HashMap<String,Object>> transactions){
+    public Block(long index, String previousBlockHash,
+                 List<HashMap<String,Object>> transactions){
 
         /*Assign values that do not need calculation*/
         this.index = index + 1;
@@ -58,13 +56,13 @@ public class Block {
                 hashInput.append(entry.getKey().toString());
                 hashInput.append(entry.getValue().toString());
             }
-            hash = digest.digest(hashInput.toString().getBytes(StandardCharsets.UTF_8));
+            hash = digest.digest(hashInput.toString().getBytes());
             initialHashes.add(DatatypeConverter.printHexBinary(hash));
         }
 
         /*Now calculate the root of the merkle tree from the hashes*/
         int pos;
-        String combinedHash = "";
+        String combinedHash;
         while(initialHashes.size() > 1){
             pos = 0;
             for(int i =0; i < initialHashes.size(); i = i+2 ){
@@ -74,7 +72,7 @@ public class Block {
                 else{
                     combinedHash = initialHashes.get(i) + initialHashes.get(i+1);
                 }
-                hash = digest.digest(combinedHash.getBytes(StandardCharsets.UTF_8));
+                hash = digest.digest(combinedHash.getBytes());
                 initialHashes.set(pos,DatatypeConverter.printHexBinary(hash));
                 pos++;
             }
@@ -84,8 +82,56 @@ public class Block {
 
         transactionHash = initialHashes.get(0);
 
-
         timestamp = System.currentTimeMillis();
+
+        blockSize = calculateSize();
+
+        System.out.println(blockSize);
+    }
+
+    /*TODO: Make this dynamic based on object variables, without any prior knowledge*/
+    private long calculateSize(){
+
+        /*Static variables*/
+        /*both hashes have size 32Bytes, so 64Bytes all in all*/
+        /*3 * 8 bytes for the long fields = 24 bytes*/
+        /* + 4 bytes the int*/
+        /*So 32 + 32 + 8 + 8 + 8 + 4 = 92 bytes*/
+        long size = 92;
+
+        for(HashMap<String,Object> transaction : transactions){
+            for(Map.Entry entry : transaction.entrySet()){
+                try {
+                    size = size + entry.getKey().toString().getBytes("UTF-8").length;
+                }
+                catch(UnsupportedEncodingException ex){
+                    ex.printStackTrace();
+                    System.exit(1);
+                }
+                Object value = entry.getValue();
+                if(value instanceof Double){
+                    size += 8;
+                }
+                else if(value instanceof Long){
+                    size += 8;
+                }
+                else if(value instanceof Integer){
+                    size += 4;
+                }
+                else if(value instanceof String){
+                    try {
+                        size = size + entry.getKey().toString().getBytes("UTF-8").length;
+                    }
+                    catch(UnsupportedEncodingException ex){
+                        ex.printStackTrace();
+                        System.exit(1);
+                    }
+                }
+            }
+        }
+
+
+        return size;
     }
 
 }
