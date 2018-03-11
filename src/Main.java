a5378b5af9a0943a896136e5c38aeb6637d7de9e
shalantor <georgkaraolanis@gmail.com>
2018-03-11 13:53:27 +0200

    Implement test for transaction hashing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import structure.Block;
 import structure.TransactionManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         TransactionManager transaction = new TransactionManager();
-        transaction.createRandomTransaction();
+
+
+        /*Test block*/
+        ArrayList<HashMap<String,Object>> transactions = new ArrayList<>();
+        for(int i =0; i < 10; i ++){
+            transactions.add(transaction.createRandomTransaction());
+        }
+        Block block = new Block(1,"qwe",transactions);
+
     }
 }
