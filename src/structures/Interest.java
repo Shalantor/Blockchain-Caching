36949b85ed7c970cd81e357fb1799ad5359b46da
shalantor <georgkaraolanis@gmail.com>
2018-03-12 16:18:15 +0200

    Getters/Setters and other methods for Interest class

--- src/structures/Interest.java
+++ src/structures/Interest.java
@@ -8,14 +8,47 @@ public class Interest {
     private String name;
     private List<Block> blocks = new ArrayList<>();
     private int weight;
+    private long size;
 
     public Interest(String name,int weight){
         this.name = name;
         this.weight = weight;
     }
 
+    public Interest(String name,int weight, List<Block> initialBlocks){
+        this(name,weight);
+        blocks = initialBlocks;
+    }
+
     public List<Block> getBlocks() {
         return blocks;
     }
 
+    public void calculateBlocksSize(){
+        long size = 0;
+        for(Block block : blocks){
+            size += block.blockSize;
+        }
+        this.size = size;
+    }
+
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    public long getSize(){
+        return size;
+    }
+
+    public void addBlock(Block block){
+        blocks.add(block);
+    }
+
+    public void removeBlock(int index){
+        blocks.remove(index);
+    }
+
+    public String getName(){
+        return name;
+    }
 }
