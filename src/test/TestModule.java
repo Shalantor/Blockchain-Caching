4d43d3c167a05c6b9641cb96ca3868ba9e3883c8
shalantor <georgkaraolanis@gmail.com>
2018-03-15 17:04:22 +0200

    Move tests to separate test modules

---
+++ src/test/TestModule.java
@@ -0,0 +1,87 @@
+package test;
+
+import nodes.LightNode;
+import nodes.NormalNode;
+import structures.Block;
+import structures.TransactionManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TestModule{
+
+    public static final int JUST_CHECK_FUNCTIONALITY = 0;
+
+    private int testType;
+    public TestModule(int testType){
+        this.testType = testType;
+    }
+
+    public void startTest(){
+        if(testType == JUST_CHECK_FUNCTIONALITY){
+            TransactionManager manager = new TransactionManager("src/test/resources/example.txt");
+
+
+            /*Test block*/
+            ArrayList<HashMap<String,Object>> transactions = new ArrayList<>();
+        /*for(int i =0; i < 10; i ++){
+            transactions.add(manager.createRandomTransaction());
+        }*/
+
+            /*Test full node*/
+        /*FullNode fullNode = new FullNode(new Block(0,"qwe",transactions));
+
+        for(int i =0; i < 10; i ++){
+            fullNode.addBlock(new Block(i+1,"",transactions));
+        }
+
+        ArrayList<Integer> indices = new ArrayList<>();
+        indices.add(0);
+        indices.add(4);
+        indices.add(8);
+        indices.add(8);
+
+        List<Block> blocks =  fullNode.getBlocksInIntervals(indices);
+
+        for(Block block: blocks){
+            System.out.println(block.index);
+        }
+        MinerNode miner = new MinerNode(new Block(0,"genesis",transactions),
+                "src/test/resources/miner.txt",manager.getKeys());
+
+        for(int i =0; i < 50; i ++){
+            miner.addTransaction(transactions.get(0));
+        }*/
+            NormalNode normal = new NormalNode("src/test/resources/normal_node_config.txt",
+                    "src/test/resources/normal_node_interests.txt");
+
+
+            transactions.add(manager.createTransaction(new ArrayList<>(
+                    Arrays.asList("ababa","bonnie","pilafi",15.4,"pizza",190)
+            )));
+
+            transactions.add(manager.createTransaction(new ArrayList<>(
+                    Arrays.asList("ababa","bonnie","pilafi",15.4,"batman",10)
+            )));
+
+            //System.out.println(transactions);
+
+            //normal.printInterests();
+
+            Block block = new Block(0,"genesis",transactions);
+            //System.out.println(block);
+
+            //normal.checkBlock(block);
+            LightNode lightNode = new LightNode("src/test/resources/normal_node_config.txt",
+                    "src/test/resources/normal_node_interests.txt");
+
+            //lightNode.printInterests();
+            lightNode.checkBlock(block);
+            normal.checkBlock(block);
+            lightNode.printBlocks();
+            normal.printBlocks();
+
+        }
+    }
+}
