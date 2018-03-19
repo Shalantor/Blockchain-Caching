16f1f57109414b0363c7ccad501a373949e283e2
shalantor <georgkaraolanis@gmail.com>
2018-03-19 12:28:50 +0200

    Add json.org library

--- src/nodes/Node.java
+++ src/nodes/Node.java
@@ -1,4 +1,14 @@
 package nodes;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class Node {
+
+    /*Read configuration from text file*/
+    public Node(String configFilePath){
+
+    }
+
 }
