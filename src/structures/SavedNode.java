d4a402acfd92d93511fe8a9b6005eae0230a79e8
shalantor <georgkaraolanis@gmail.com>
2018-03-28 18:31:50 +0300

    Add new class to save info from nodes responding to interest request

---
+++ src/structures/SavedNode.java
@@ -0,0 +1,15 @@
+package structures;
+
+public class SavedNode {
+
+    public String host;
+    public int port;
+    public int score;
+
+    public SavedNode(String host,int port, int score){
+        this.host = host;
+        this.port = port;
+        this.score = score;
+    }
+
+}
