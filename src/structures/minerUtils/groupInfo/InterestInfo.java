104b10d316bfb3157e0762013ce78a352c77f9c9
shalantor <georgkaraolanis@gmail.com>
2018-04-10 15:35:54 +0300

    Return value based on type

--- src/structures/minerUtils/groupInfo/InterestInfo.java
+++ src/structures/minerUtils/groupInfo/InterestInfo.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class InterestInfo {
 
+    /*Possible names for type values*/
+    private static final String STRING = "string";
+    private static final String DOUBLE = "double";
+    private static final String INTEGER = "integer";
+    private static final String LONG = "long";
+    private static final String GREATER = "greater";
+    private static final String LOWER = "lower";
+
     /*This info is for all interests*/
     private ArrayList<Integer> indices;
     private String type; /*Integer, Double, Long or String*/
@@ -51,6 +59,15 @@ public class InterestInfo {
     }
 
     public Object getValue() {
+        if(type.equals(DOUBLE)){
+            return (Double) value;
+        }
+        else if(type.equals(INTEGER)){
+            return (Integer) value;
+        }
+        else if(type.equals(LONG)){
+            return (Long) value;
+        }
         return value;
     }
 
