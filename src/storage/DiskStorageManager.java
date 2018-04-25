e072104ba32f2ea63d6a1996a2819906d874942a
shalantor <georgkaraolanis@gmail.com>
2018-04-25 17:51:00 +0300

    Implement get blocks from interest request

--- src/storage/DiskStorageManager.java
+++ src/storage/DiskStorageManager.java
@@ -4,11 +4,12 @@ import com.mongodb.*;
 import com.mongodb.util.JSON;
 import nodes.Node;
 import org.json.JSONObject;
+import storage.storageUtils.BlockExplorer;
 import structures.Block;
+import structures.Interest;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class DiskStorageManager extends StorageManager{
 
@@ -16,6 +17,10 @@ public class DiskStorageManager extends StorageManager{
     private final static String DATABASE_NAME = "Blockchain";
     private final static String DATABASE_ADDRESS = "localhost";
     private final static String COLLECTION_NAME = "blockchain";
+    private static final String STRING = "string";
+    private static final String DOUBLE = "double";
+    private static final String INTEGER = "integer";
+    private static final String LONG = "long";
 
     /*Database connection*/
     private DBCollection dbCollection;
@@ -88,6 +93,72 @@ public class DiskStorageManager extends StorageManager{
     @Override
     public int getSize(){
         return size;
+    }
+
+    @Override
+    public ArrayList<Block> getBlockFromInterests(ArrayList<Interest> interests){
+
+        ArrayList<Block> blocks = new ArrayList<>();
+
+        for(Interest i : interests){
+            if(i.type == Interest.STRING_TYPE){
+
+                /*values that the node is interested in*/
+                BasicDBList criteria = new BasicDBList();
+                for(String value : i.interestValues){
+                    criteria.add(new BasicDBObject("transactions." + i.interestName,value));
+                }
+                BasicDBObject query = new BasicDBObject("$or",criteria);
+                DBCursor cursor = dbCollection.find(query);
+                while (cursor.hasNext()){
+                    JSONObject jsonObject = new JSONObject(cursor.next().toString().trim());
+                    jsonObject.remove("_id");
+                    blocks.add(new Block(jsonObject,node));
+                }
+            }
+            else if(i.type == Interest.NUMERIC_TYPE){
+
+                /*Check type*/
+                Object value = null;
+                String name = i.interestName;
+                if(types.get(name).equals(DOUBLE)){
+                    value = (Double) i.numericValue;
+                }
+                else if(types.get(name).equals(LONG)){
+                    value = (Long) i.numericValue;
+                }
+                else if(types.get(name).equals(INTEGER)){
+                    value = (Integer) i.numericValue;
+                }
+
+                String numericOperator = "";
+
+                if(i.numericType == Interest.NUMERIC_GREATER){
+                    numericOperator = "$gt";
+                }
+                else{
+                    numericOperator = "$lt";
+                }
+
+                /*numeric query*/
+                BasicDBObject query = new BasicDBObject(numericOperator,value);
+
+                DBCursor cursor = dbCollection.find(new BasicDBObject("transactions." + i.interestName,query));
+
+                /*Add to blocks to return*/
+                while (cursor.hasNext()){
+                    JSONObject jsonObject = new JSONObject(cursor.next().toString().trim());
+                    jsonObject.remove("_id");
+                    blocks.add(new Block(jsonObject,node));
+                }
+
+            }
+        }
+
+        /*Now sort*/
+        blocks.sort(Block::compareTo);
+
+        return blocks;
     }
 
 }
