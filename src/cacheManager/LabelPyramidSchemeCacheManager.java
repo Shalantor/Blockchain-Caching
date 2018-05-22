1fe4c74e000f28c7127c972c32119a6c1b9855e1
shalantor <georgkaraolanis@gmail.com>
2018-05-22 12:34:58 +0300

    Starter code for new cache manager

---
+++ src/cacheManager/LabelPyramidSchemeCacheManager.java
@@ -0,0 +1,138 @@
+package cacheManager;
+
+import cacheManager.CacheUtils.HitRateCostBlock;
+import nodes.Node;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import structures.Block;
+import structures.Interest;
+import structures.SavedNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LabelPyramidSchemeCacheManager extends CacheManager{
+
+    private long timeLimit;
+    private long cacheSize;
+    private long sizeOfCachedBlocks;
+    private ArrayList<HitRateCostBlock> blocksInCache;
+
+    public LabelPyramidSchemeCacheManager(long timeLimit, long cacheSize) {
+        this.timeLimit = timeLimit;
+        this.cacheSize = cacheSize;
+        blocksInCache = new ArrayList<>();
+        bestNodes = new ArrayList<>();
+    }
+
+    @Override
+    public boolean checkBlock(Block block, Map<String,Interest> interests){
+        for (Map.Entry entry : interests.entrySet()){
+            if(((Interest)entry.getValue()).checkBlock(block)){
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    /*TODO: make nodes use this*/
+    @Override
+    public void evaluateInterests(JSONObject receivedInterests,
+                                  HashMap<String,Interest> ownInterests,
+                                  Node node){
+
+        /*Best case scenario is when the manager finds interests
+         * that are exactly the same with its own interests. Else, find
+         * interests with as little difference as possible*/
+        JSONArray interests = receivedInterests.getJSONArray("interests");
+
+        Interest interest,ownInterest;
+        int matches=0,missMatches=0;
+
+        for(int i = 0; i < interests.length(); i++){
+
+            /*Extract interest*/
+            interest = node.JSONToInterest(interests.getJSONObject(i));
+
+            /*Is there an interest like this in my own interests?*/
+            if(ownInterests.containsKey(interest.interestName)){
+                ownInterest = ownInterests.get(interest.interestName);
+
+                /*String type?*/
+                if(interest.type == Interest.STRING_TYPE){
+                    for(String value: interest.interestValues){
+                        if(ownInterest.interestValues.contains(value)){
+                            matches += 1;
+                        }
+                        else{
+                            missMatches += 1;
+                        }
+                    }
+                    /*Different size means that there are different interests*/
+                    if(interest.interestValues.size() < ownInterest.interestValues.size()){
+                        missMatches += ownInterest.interestValues.size() - interest.interestValues.size();
+                    }
+                }
+                /*or numeric type?. Numeric are simpler*/
+                else if(interest.type == Interest.NUMERIC_TYPE){
+                    if(interest.numericType == ownInterest.numericType){
+                        String interestString,ownInterestString;
+                        interestString = interest.numericValue + "";
+                        ownInterestString = ownInterest.numericValue + "";
+                        if(interestString.equals(ownInterestString)){
+                            matches += 1;
+                        }
+                        else{
+                            missMatches += 1;
+                        }
+                    }
+                }
+            }
+            else{
+                missMatches += 2;
+            }
+        }
+
+        /*Now check matches and miss matches and compare to others*/
+        SavedNode savedNode = new SavedNode(receivedInterests.getString("host"),
+                receivedInterests.getInt("port"), matches - missMatches);
+
+        /*Is list empty? then just add, else insert in sorted list*/
+        if(bestNodes.isEmpty()){
+            bestNodes.add(savedNode);
+        }
+        else{
+            for(int i =0; i < bestNodes.size(); i++){
+                if(bestNodes.get(i).score <= savedNode.score){
+                    bestNodes.add(i,savedNode);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void removeSavedNodes(){
+        bestNodes.clear();
+    }
+
+    public ArrayList<Block> getBlocksInCache() {
+        ArrayList<Block> blocks = new ArrayList<>();
+        for(HitRateCostBlock b : blocksInCache){
+            blocks.add(b.getBlock());
+        }
+        return blocks;
+    }
+
+    public long getSizeOfCachedBlocks() {
+        return sizeOfCachedBlocks;
+    }
+
+    @Override
+    public void clearAll(){
+        sizeOfCachedBlocks = 0;
+        blocksInCache.clear();
+    }
+}
